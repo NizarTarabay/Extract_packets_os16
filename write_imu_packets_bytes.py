29271a7198465d1de08f0b4db05dfbd39568cf4f
Nizar <nizar.trb9@gmail.com>
2019-08-29 11:14:04 -0400

    add directory for the saving files

--- write_imu_packets_bytes.py
+++ write_imu_packets_bytes.py
@@ -1,6 +1,8 @@
 import socket
 import time
 import keyboard
+import os
+import datetime
 UDP_IP = '10.5.5.1'
 UDP_port_IMU = 7503
 #
@@ -9,7 +11,22 @@ sock_IMU = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock_IMU.bind((UDP_IP, UDP_port_IMU))
 #
 
-IMU_file = open('IMU_byte_packet.txt', 'wb')
+# Create target Directory if don't exist
+save_directory = "/media/nizar/Transcend/test in the lab/Data/Bytes/IMU"
+if not os.path.exists(save_directory):
+    os.mkdir(save_directory)
+    print("Directory ", save_directory, " Created ")
+else:
+    print("Directory ", save_directory, " already exists")
+os.chdir(save_directory)
+
+now = datetime.datetime.now()
+T = now.strftime("%Y-%m-%d %H:%M:%d")
+
+file_name = 'IMU_byte_packet_' + T + '.txt'
+
+
+IMU_file = open(file_name, 'wb')
 j=0
 running = True
 start = time.time()
