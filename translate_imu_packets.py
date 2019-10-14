732895a956be256b5cb2e25f362330d720ed7f72
Nizar <nizar.trb9@gmail.com>
2019-10-14 14:26:27 -0400

    translate udp imu packet to a readble format

--- translate_imu_packets.py
+++ translate_imu_packets.py
@@ -23,17 +23,17 @@ f_write = open('IMU_myFormat_packet_' + file_name_t + '.txt', 'w')
 
 f_read = f.read()
 start = time.time()
-j = 0
-for i in range(0, 2419):
-    f_write.write('IMU_T: '  + str(struct.unpack('Q', f_read[j:j+8])[0]) + ' ')  # IMU read time
-    f_write.write('Acc_T: '  + str(struct.unpack('Q', f_read[j+8:j+16])[0]) + ' ')  # Accelerometer read time
-    f_write.write('Gyro_T: ' + str(struct.unpack('Q', f_read[j+16:j+24])[0]) + '\n')  # Gyroscope read time
-    f_write.write('acc_x: '  + str(struct.unpack('f', f_read[j+24:j+28])[0]) + ' ')  # Acceleration in x-axis (g)
-    f_write.write('acc_y: '  + str(struct.unpack('f', f_read[j+28:j+32])[0]) + ' ')  # Acceleration in y-axis (g)
-    f_write.write('acc_z: '  + str(struct.unpack('f', f_read[j+32:j+36])[0]) + ' ')  # Acceleration in z-axis (g)
-    f_write.write('angV_x: ' + str(struct.unpack('f', f_read[j+36:j+40])[0]) + ' ')  # Angular velocity in x-axis
-    f_write.write('angV_y: ' + str(struct.unpack('f', f_read[j+40:j+44])[0]) + ' ')  # Angular velocity in y-axis
-    f_write.write('angV_z: ' + str(struct.unpack('f', f_read[j+44:j+48])[0]) + '\n')  # Angular velocity in z-axis
+
+for i in range(0, int(len(f_read)/48)):
+    f_write.write('IMU_T: ' + str(struct.unpack('Q', f_read[48*i: 48*i + 8])[0]) + ' ')  # IMU read time
+    f_write.write('Acc_T: ' + str(struct.unpack('Q', f_read[48*i + 8: 48*i + 16])[0]) + ' ')  # Accelerometer read time
+    f_write.write('Gyro_T: ' + str(struct.unpack('Q', f_read[48*i + 16: 48*i + 24])[0]) + '\n')  # Gyroscope read time
+    f_write.write('acc_x: ' + str(struct.unpack('f', f_read[48*i + 24: 48*i + 28])[0]) + ' ')  # Acceleration in x-axis (g)
+    f_write.write('acc_y: ' + str(struct.unpack('f', f_read[48*i + 28: 48*i + 32])[0]) + ' ')  # Acceleration in y-axis (g)
+    f_write.write('acc_z: ' + str(struct.unpack('f', f_read[48*i + 32: 48*i + 36])[0]) + ' ')  # Acceleration in z-axis (g)
+    f_write.write('angV_x: ' + str(struct.unpack('f', f_read[48*i + 36: 48*i + 40])[0]) + ' ')  # Angular velocity in x-axis
+    f_write.write('angV_y: ' + str(struct.unpack('f', f_read[48*i + 40: 48*i + 44])[0]) + ' ')  # Angular velocity in y-axis
+    f_write.write('angV_z: ' + str(struct.unpack('f', f_read[48*i + 44: 48*i + 48])[0]) + '\n')  # Angular velocity in z-axis
 
 end = time.time()
 elapse = end - start
