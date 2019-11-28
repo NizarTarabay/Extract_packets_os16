e2e7aebad5574ea9cf7e55bdab1953fc6a00c773
Nizar <nizar.trb9@gmail.com>
2019-11-27 20:26:05 -0500

    cartezian and mse calculated

--- helpers.py
+++ helpers.py
@@ -273,10 +273,189 @@ def crop_image (output_size = 640):
 
     os.chdir(path_selected)
     for filename in glob.glob("*.png"):
-        img = cv2.imread(filename, 0)
+        img = cv2.imread(filename)
         for i in range(0, 3):
             crop_img = img[0: img.shape[0], output_size * i : output_size * i + output_size]
             crop_img_resize = cv2.resize(crop_img, (output_size, output_size), interpolation=cv2.INTER_AREA)
             path = '/media/nizar/Transcend/cocosynth/datasets/pavement_distress_synthetic/input/backgrounds/images_crp'
             filename_crp = str(i) + '_' + filename
             cv2.imwrite(os.path.join(path, filename_crp), crop_img_resize)
+
+
+
+def get_frame(file, mode, signal_name, frame_id):
+    '''
+    this function fetch a specific frame according to its frame id
+    :param file: myFormat file name
+    :param mode: 512, 1021, 2048
+    :param signal_name: string; which signal you are looking after;
+    :param frame_id: the requested frame
+    :return: array; the array contains the signal value of a specific frame
+    '''
+    frame = get_signal(file, mode, signal_name)[frame_id]
+    return frame
+
+
+def frame_drop_checker (line_list):
+    '''
+    This function check myFormat file for any dropping packets and fromes, it shows a graph of where the packages are
+    dropped and how many frames are missed
+    :param line_list: List of all the lines in the myFormat file
+    :return:
+    '''
+
+    lis = frame_list(line_list)
+    frame = []
+    index = []
+    i = 0
+    j = 0
+    element_old_old = 0
+    element_old = lis[0]
+    for element in lis:
+
+        if element_old == element:
+            i += 1
+            element_old = element
+        else:
+            frame.append(i)
+            i=0
+            if element-element_old > 1 and j != 0:
+                print ('dropping frames:', element-element_old )
+                idx = lis.index(element_old)
+                index.append(idx)
+                element_old_old = element_old
+            # else:
+                # print ('no frame losses')
+            element_old = element
+        j += 1
+
+    plt.plot(frame)
+    plt.ylabel('number of frames')
+    plt.show()
+
+
+def spherical_coordinate_data( l, line_list, spherical_coordinate_array, k, enc, i, channel_angle):
+    '''
+    This function get access to the lines that contains the channels' signals and extract the requested signal from them
+    :param signal_name: string; which signal you are looking after;
+    can take the following values:'range', 'reflectivity', 'signal', 'ambient'
+    :param l: this is an index to iterate between the list of the lines that belong to myFormat file
+    :param line_list: List of all the lines in the myFormat file
+    :param img_array: empty, a 3 dimensional array:
+    total number of frames x (512 or 256 depending on the mode) x 16 channels
+    :param k: to iterate between frames k in range (0: total number of frames)
+    :param enc: get the encoder value to locate the 16 channels in each frame enc in range (0: 512) or (0: 256)
+    :param i: to iterate between the 16 channels
+    :return: img_array: full, a 3 dimensional array:
+    total number of frames x (512 or 256 depending on the mode) x 16 channels
+    '''
+    enc_min = 33792
+    #encoder = (enc * 44 * (2048/mode) + enc_min)*360 / 90112
+    encoder = -(enc * 44 * (2048/mode) + enc_min)*360 / 90112 +360
+    # encoder = (-(360 / 90112) * enc * 44 * (2048/mode) + 360) +224
+    signal = ['0']
+    s = 0
+    for c in line_list[l]:
+        if s == 0 and (c != ' ' or c != '\n'):  # s=1 or 0 1 for reflectivity 0 for range
+            signal.append(c)
+        if c == ' ':
+            s += 1
+        if s == 1:  # s=1 or 2; 2 for reflectivity 1 for range
+            break
+    sig = ''
+    sig = int(sig.join(signal))
+    spherical_coordinate_array[k][int(enc)][i - 1] = [sig, encoder, 90 + 00 - channel_angle]  # (rho, theta, phi)
+    return spherical_coordinate_array
+
+def get_spherical_coordinate(img_array_depth, line_list):
+    '''
+    This function get access to the lines in the myFormat file and extract the information related to the encoder,
+    to the frame, and to the signal
+    :param file: myFormat file name
+    :param mode: 512, 1021, 2048
+    :param signal_name: string; which signal you are looking after;
+    :return: img_array: full, a 3 dimensional array:
+    total number of frames x (512 or 256 depending on the mode) x 16 channels
+    '''
+
+    spherical_coordinate_array = np.zeros((img_array_depth+1, int(mode / 4) , 16), dtype=list)
+    # this is the array the contains all the pixels acquired by the sensor
+    m, i, j, k, l = 0, 0, 0, 0, 0
+
+    enc_min = smallest_encoder_tick(line_list)
+    framelist = frame_list(line_list)[0]
+
+    for k in range(0, img_array_depth+1):
+        for j in range(0, int(mode / 4) ):
+            for i in range(0, 18):
+                if l % 18 == 0:
+                    encoder_count = ['0']
+                    s = 0
+                    for c in line_list[l]:
+                        if s == 5 and (c != ' ' or c != '\n'):  # s=3 encoder don't touch!
+                            encoder_count.append(c)
+                        if c == ' ':
+                            s += 1
+                        if s == 6:  # s=4 encoder don't touch!
+                            break
+                    enc = ''
+                    enc = (int(enc.join(encoder_count)) - enc_min) / (44 * (2048 / mode))
+                    real_frame = first_last_frame_number(line_list[l])  # add
+                else:
+                    if (l + 1) % 18 != 0:
+                        channel_angle = 14.8-(i)*2.006
+                        # img_array = signal_data(signal_name, l, linelist, img_array, k, enc, i)
+                        spherical_coordinate_array = spherical_coordinate_data(l, line_list, spherical_coordinate_array,
+                                                                          real_frame - framelist, enc, i, channel_angle)
+
+                l += 1
+                if l >= len(line_list):
+                    break
+            if l >= len(line_list):
+                break
+        if l >= len(line_list):
+            break
+    return spherical_coordinate_array
+
+
+def spherical_to_cartesian (spherical_coordinate_array, Lidar_direction, height):
+    '''
+
+    :param spherical_coordinate_array:
+    :param Lidar_direction: angle in degree between the lidar and the ground (54)
+    :param height: the heigt of the lidar abobe the ground
+    :return: cartezian (xyz) spherical array and the z channel
+    '''
+    cartesian_coordinate_array = np.zeros ((spherical_coordinate_array.shape[0], spherical_coordinate_array.shape[1],
+                         spherical_coordinate_array.shape[2]), dtype= list)
+    z_array = np.zeros ((spherical_coordinate_array.shape[0], spherical_coordinate_array.shape[1],
+                         spherical_coordinate_array.shape[2]))
+
+    for i in range(0, spherical_coordinate_array.shape[0]):
+        for j in range(0, spherical_coordinate_array.shape[1]):
+            for k in range(0, spherical_coordinate_array.shape[2]):
+                if str(type(spherical_coordinate_array[i][j][k])) == "<class 'list'>":
+
+                    rho = spherical_coordinate_array[i][j][k][0]
+                    theta = math.radians(spherical_coordinate_array[i][j][k][1])
+                    phi = math.radians(spherical_coordinate_array[i][j][k][2])
+                    print (spherical_coordinate_array[i][j][k][2])
+                    x = rho * math.sin(phi) * math.cos(theta)
+                    y = rho * math.sin(phi) * math.sin(theta)
+                    z = rho * math.cos(phi)
+                    u = math.cos(math.radians(-Lidar_direction)) * x + math.sin(math.radians(-Lidar_direction)) * z
+                    v = y
+                    w = -math.sin(math.radians(-Lidar_direction)) * x + math.cos(math.radians(-Lidar_direction)) * z + height
+                    cartesian_coordinate_array[i][j][k] = [u, v, w]
+                    z_array[i][j][k] = w
+                else:
+                    cartesian_coordinate_array[i][j][k] = [0, 0, 0]
+
+    return cartesian_coordinate_array, z_array
+
+# def packet_checker ():
+#     '''
+#
+#     :return: this function returns a boolean indicates if the packet is good or bad
+#     '''
+#
